b4dd06a9542e22aa2050982617b79914ef05ea04
craigfili <craig.filipowicz@gmail.com>
2024-08-19 22:20:51 -0500

    Finish Studio

---
+++ part1/studio/src/components/Description.jsx
@@ -0,0 +1,33 @@
+import styles from './Description.module.css'
+import React from 'react'
+let recipeTitle = "White Chocolate Oatmeal Cookie";
+let recipeDescription = "The most delicious, chewy blueberry oatmeal cookies with sweet white chocolate and coconut in every bite. These easy blueberry oatmeal cookies are made with coconut oil for double the coconut flavor and seriously taste like a fresh blueberry muffin. They’re my husband’s absolute favorite cookie!"
+
+const RecipeAuthor = () => {
+    let authorLink = "https://www.ambitiouskitchen.com/white-chocolate-blueberry-coconut-oatmeal-cookies/";
+    let authorPhoto = "https://www.ambitiouskitchen.com/wp-content/uploads/2020/12/ak-mini-bio-2a-1.jpg"; 
+    let authorName = "Monique Volz";
+    return (
+        <div className = {styles.recipeAuthorBlock}>
+           <img src={authorPhoto} alt = {authorName} className={styles.imageUpdates} />
+           <div>
+              <h3>{authorName}</h3>
+              <a href={authorLink}>Ambitious Kitchen</a> 
+           </div>
+        </div>
+     ); 
+}
+    class RecipeDescription extends React.Component {
+        render(){
+            return(
+                <div> 
+                 <div>
+                  <h1>{recipeTitle}</h1>
+                    <p>{recipeDescription}</p>
+                 </div>
+                 <RecipeAuthor />
+                </div>
+            )
+        }
+    }
+    export default RecipeDescription;
---
+++ part1/studio/src/components/Photos.jsx
@@ -0,0 +1,15 @@
+import styles from './Description.module.css' 
+
+const RecipePhoto = () => { 
+    return (
+        <img
+        src="https://www.ambitiouskitchen.com/wp-content/uploads/2019/12/blueberrycookies-2.jpg"
+        alt="Blueberry White Chocolate Oatmeal Cookies"
+        className={styles.imageUpdates}
+      />
+
+    )
+};
+
+
+export default RecipePhoto;
